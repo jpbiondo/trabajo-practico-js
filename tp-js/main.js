8a895c2b187aa325f50a208554724ce0bc2b78d2
Juan Pablo Biondo <jupbiondo@gmail.com{>
2024-10-07 22:33:50 -0300

    Añadir ejercicio 1 de la seccion 3

--- tp-js/main.js
+++ tp-js/main.js
@@ -2,6 +2,7 @@ import './style.css'
 // import './ejercicios/introduccion/ejercicio2'
 // import './ejercicios/introduccion/ejercicio3'
 // import './ejercicios/2operadoresLogYCond/ejercicio1'
-import './ejercicios/2operadoresLogYCond/ejercicio2'
+// import './ejercicios/2operadoresLogYCond/ejercicio2'
+import './ejercicios/3opAsignYBucles/ejercicio1'
 
 

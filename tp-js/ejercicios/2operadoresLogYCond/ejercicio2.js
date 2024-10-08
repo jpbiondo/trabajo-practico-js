ac5d408e3ed5b58dc9dc6879761bbb206b475aab
Juan Pablo Biondo <jupbiondo@gmail.com{>
2024-10-07 22:29:27 -0300

    Añadir ejercicio 2 de seccion 2

---
+++ tp-js/ejercicios/2operadoresLogYCond/ejercicio2.js
@@ -0,0 +1,6 @@
+let number = Number(prompt("Ingrese un numero"));
+if(number % 2 == 0) {
+    console.log("El número", number, "es par");
+} else {
+    console.log("El número", number, "es impar");
+}
--- tp-js/main.js
+++ tp-js/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 // import './ejercicios/introduccion/ejercicio2'
 // import './ejercicios/introduccion/ejercicio3'
-import './ejercicios/2operadoresLogYCond/ejercicio1'
+// import './ejercicios/2operadoresLogYCond/ejercicio1'
+import './ejercicios/2operadoresLogYCond/ejercicio2'
 
 

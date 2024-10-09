bbb898d7232da063f663b35ca5ed182ead47b726
Juan Pablo Biondo <jupbiondo@gmail.com{>
2024-10-09 09:21:17 -0300

    Update ejercicio 7 añadiendo clases CSS

--- tp-js/ejercicios/7introDom/ejercicio1.js
+++ tp-js/ejercicios/7introDom/ejercicio1.js
@@ -1,7 +1,7 @@
-const btn = document.querySelector("button");
+const btn = document.querySelector(".btn1");
 
 btn.addEventListener("click", () => {
-    const paragraphs = document.querySelectorAll("p");
+    const paragraphs = document.querySelectorAll(".p1");
     paragraphs.forEach((paragraph) => {
         paragraph.style.color = 'blue';
     })
